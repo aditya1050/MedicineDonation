15f3573380bbe9e87d53045c39bd9175d8a91799
Aditya Kumar <adityakumar50505@gmail.com>
2021-01-10 19:08:29 +0530

    MedicineDonationApp

---
+++ home/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from home.models import Userngo,Userdonor,Donations
+# Register your models here.
+admin.site.register(Userngo)
+admin.site.register(Userdonor)
+admin.site.register(Donations)
